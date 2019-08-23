673b40b4ae14de670945de50083501eb51ae755a
Zolton <gmzesq@gmail.com>
2019-08-23 15:19:51 -0400

    Added some more testing, playing around with tokens in the promisory loop.  Still works

--- api/server.spec.js
+++ api/server.spec.js
@@ -13,82 +13,59 @@ describe("GET request", () => {
   });
 });
 
-describe("login", ()=>{
-    describe("login agian", ()=>{
-        it("should work", async()=>{
-            return await request(server)
-            .post("/api/auth/register")
-            .send({username: "FinalTest", password: "FinalTest"})
+describe("login", () => {
+  describe("login agian", () => {
+    it("should work", async () => {
+      return await request(server)
+        .post("/api/auth/register")
+        .send({ username: "FinalTest7", password: "FinalTest7" })
+        .expect(200)
+        .then(res => {
+          request(server)
+            .post("/api/auth/login")
+            .send({ username: "FinalTest7", password: "FinalTest7" })
             .expect(200)
-            .then(res=>{
-                request(server)
-                .post("/api/auth/login")
-                .send({username: "FinalTest", password: "FinalTest"})
-                .expect(200)
-            })
-        })
-    })
-})
-
-// describe("register request", () => {
-//   it.skip("register success", () => {
-//     return request(server)
-//       .post("/api/auth/register")
-//       .send({ username: "API2", password: "password2" })
-//       .then(res => {
-//         expect(res.type).toMatch(/json/);
-//         expect(res.status).toBe(200);
-//       });
-//   });
-//   it("register fail", () => {
-//     return (
-//       request(server)
-//         .post("/api/auth/register")
-//         // no password - should return 500
-//         .send({ username: "API 123 Test2" })
-//         .then(res => {
-//           expect(res.status).toBe(500);
-//         })
-//     );
-//   });
-// });
-
-// describe("login success", () => {
-//   it("login success", () => {
-//     return request(server)
-//       .post("/api/auth/login")
-//       .send({
-//         username: "Bilbo",
-//         password: "Bilbo"
-//       })
-//       .expect(200);
-//   });
-//   it("login fail", () => {
-//     return request(server)
-//       .post("/api/auth/login")
-//       .set("Content-Type", "application/json")
-//       .send({ username: "API 123 Test2" })
-//       .then(res => {
-//         expect(res.status).toBe(401);
-//       });
-//   });
-// });
-
-describe.skip("Gets jokes", () => {
-  it("joke fetch succeed", () => {
-    return request(server)
-      .get("/api/jokes")
-      .set({
-        Authorization:
-          eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-            .eyJzdWJqZWN0IjozLCJ1c2VybmFtZSI6IkJpbGJvIiwiaWF0IjoxNTY2NTc5NzgyLCJleHAiOjE1NjY2NjYxODJ9
-            .LysMyEKvBZpKF4QyHn - iqhVOfkQXsllAhMlzI_TdAkU
-      })
-      .expect(200);
+            .then(res => {
+              request(server)
+                .get("/api/jokes")
+                .set({
+                  Authorization:
+                    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+                      .eyJzdWJqZWN0IjozLCJ1c2VybmFtZSI6IkJpbGJvIiwiaWF0IjoxNTY2NTg2MTU1LCJleHAiOjE1NjY2NzI1NTV9
+                      .tzzPWj6EmG0L53OFssRYp_zjIl6662pxskzjz5yRgxY
+                })
+                .expect(200);
+            });
+        });
+    });
   });
+});
+
+describe("register request", () => {
+  it("register fail", () => {
+    return request(server)
+      .post("/api/auth/register")
+      .send({ username: "API 123 Test2" })
+      .then(res => {
+        expect(res.status).toBe(500);
+      });
+  });
+});
+
+describe("login fail", () => {
+  it("login fail2", () => {
+    return request(server)
+      .post("/api/auth/login")
+      .send({ username: "API 123 Test2" })
+      .expect(500);
+  });
+});
+
+describe("Gets jokes", () => {
   it("joke fetch fail", () => {
     return request(server)
       .get("/api/jokes")
+      .send({ username: "API 123 Test2" })
       .then(res => {
         expect(res.status).toBe(401);
       });
