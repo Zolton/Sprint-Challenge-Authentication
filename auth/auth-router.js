f1e59d52b709e11c2351c9ec0619f039aa94d03d
Zolton <gmzesq@gmail.com>
2019-08-23 12:44:50 -0400

    Weird bugs

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -39,29 +39,54 @@ function generateToken(user) {
     subject: user.id,
     username: user.username
   };
-  const secret = process.env.secret
+  const secret = process.env.secret;
   const options = {
     expiresIn: "1d"
-  }
-  return jwt.sign(payload, secret, options)
+  };
+  return jwt.sign(payload, secret, options);
+}
+
+// function findBy (user) {
+//   return db("users").where({username: user})
+// }
+
+// router.post("/find", (req, res)=>{
+//   let username = req.body.username
+//   let password = req.body.password
+//   //let { username, password } = req.body;
+//   findUser(username).then(test=>{
+//     res.status.json(test)
+//   })
+//   .catch(error => {
+//     res.status(500).json({ Error: "User not found", username, password });
+//   });
+// })
+
+function findUser(user) {
+  return db("users").where({ username: user });
 }
 
 router.post("/login", (req, res) => {
   // implement login
-  let {username, password} = req.body
-  db("users")
-    .where( username )
+  let username = req.body.username
+  let password = req.body.password
+  //let { username, password } = req.body;
+  findUser(username)
     .first()
     .then(user => {
+      console.log("Hello from inside .then")
+      console.log(user)
       if (user && bcrypt.compareSync(password, user.password)) {
+        console.log("Hello from inside if")
         const token = generateToken(user);
         res.status(200).json({ Welcome: "Here's your token", token });
       } else {
+        console.log("hello from inside else")
         res.status(401).json({ Error: "Password fail" });
       }
     })
     .catch(error => {
-      res.status(500).json({ Error: "User not found" });
+      res.status(500).json({ Error: "User not found", username, password, error });
     });
 });
 
